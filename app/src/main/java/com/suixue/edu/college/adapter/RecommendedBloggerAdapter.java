65fd2406e61c3c3900e271967ffeb73ac2fe83a1
670832188 <670832188.qq.com>
2018-10-10 22:59:39 +0800

    推荐博主添加最近发布的图片

--- app/src/main/java/com/suixue/edu/college/adapter/RecommendedBloggerAdapter.java
+++ app/src/main/java/com/suixue/edu/college/adapter/RecommendedBloggerAdapter.java
@@ -2,7 +2,6 @@ package com.suixue.edu.college.adapter;
 
 import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -10,7 +9,9 @@ import com.bumptech.glide.Glide;
 import com.dev.kit.basemodule.surpport.BaseRecyclerAdapter;
 import com.dev.kit.basemodule.surpport.RecyclerViewHolder;
 import com.dev.kit.basemodule.util.DisplayUtil;
+import com.dev.kit.basemodule.util.GlideUtil;
 import com.dev.kit.basemodule.util.ToastUtil;
+import com.joooonho.SelectableRoundedImageView;
 import com.suixue.edu.college.R;
 import com.suixue.edu.college.entity.RecommendedBloggerInfo;
 
@@ -46,6 +47,31 @@ public class RecommendedBloggerAdapter extends BaseRecyclerAdapter<RecommendedBl
                 tryToConcernBlogger(info.getBloggerId());
             }
         });
+        SelectableRoundedImageView ivPic1 = holder.getView(R.id.iv_pic1);
+        SelectableRoundedImageView ivPic2 = holder.getView(R.id.iv_pic2);
+        SelectableRoundedImageView ivPic3 = holder.getView(R.id.iv_pic3);
+
+        List<String> latestPictures = info.getLatestPictures();
+        if (latestPictures != null && latestPictures.size() > 0) {
+            int size = latestPictures.size();
+            if (size >= 3) {
+                GlideUtil.loadImage(context, latestPictures.get(0), R.drawable.bg_default_pic_thumb, R.drawable.bg_default_pic_thumb, ivPic1, 1);
+                GlideUtil.loadImage(context, latestPictures.get(1), R.drawable.bg_default_pic_thumb, R.drawable.bg_default_pic_thumb, ivPic2, 1);
+                GlideUtil.loadImage(context, latestPictures.get(2), R.drawable.bg_default_pic_thumb, R.drawable.bg_default_pic_thumb, ivPic3, 1);
+            } else if (size == 2) {
+                GlideUtil.loadImage(context, latestPictures.get(0), R.drawable.bg_default_pic_thumb, R.drawable.bg_default_pic_thumb, ivPic1, 1);
+                GlideUtil.loadImage(context, latestPictures.get(1), R.drawable.bg_default_pic_thumb, R.drawable.bg_default_pic_thumb, ivPic2, 1);
+                ivPic3.setImageResource(R.drawable.bg_default_pic_thumb);
+            } else {
+                GlideUtil.loadImage(context, latestPictures.get(0), R.drawable.bg_default_pic_thumb, R.drawable.bg_default_pic_thumb, ivPic1, 1);
+                ivPic2.setImageResource(R.drawable.bg_default_pic_thumb);
+                ivPic3.setImageResource(R.drawable.bg_default_pic_thumb);
+            }
+        } else {
+            ivPic1.setImageResource(R.drawable.bg_default_pic_thumb);
+            ivPic2.setImageResource(R.drawable.bg_default_pic_thumb);
+            ivPic3.setImageResource(R.drawable.bg_default_pic_thumb);
+        }
     }
 
     private void tryToConcernBlogger(String bloggerId) {

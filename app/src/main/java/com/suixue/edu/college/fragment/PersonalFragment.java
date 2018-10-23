2d5ef2d226538b113d3cd67e22746a7c879408c1
崔岩 <cuiyan@xiaomi.com>
2018-10-23 19:48:57 +0800

    个人页面添加部分布局

--- app/src/main/java/com/suixue/edu/college/fragment/PersonalFragment.java
+++ app/src/main/java/com/suixue/edu/college/fragment/PersonalFragment.java
@@ -1,9 +1,25 @@
 package com.suixue.edu.college.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.dev.kit.basemodule.fragment.BaseFragment;
+import com.suixue.edu.college.R;
 
 /**
  * Created by cuiyan on 2018/9/12.
  */
 public class PersonalFragment extends BaseFragment {
+
+    private View rootView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.frg_personal, container, false);
+        return rootView;
+    }
 }

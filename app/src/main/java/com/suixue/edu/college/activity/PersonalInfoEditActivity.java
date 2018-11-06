1f94fd0b73a163e16dd046e020ec9f97838677c9
崔岩 <cuiyan@xiaomi.com>
2018-11-06 16:00:34 +0800

    ts

--- app/src/main/java/com/suixue/edu/college/activity/PersonalInfoEditActivity.java
+++ app/src/main/java/com/suixue/edu/college/activity/PersonalInfoEditActivity.java
@@ -10,10 +10,13 @@ import com.suixue.edu.college.config.Constants;
 import com.suixue.edu.college.util.PickerUtil;
 import com.suixue.edu.college.widget.BottomSelectorDialog;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import cn.qqtheme.framework.picker.DatePicker;
+import cn.qqtheme.framework.picker.SinglePicker;
 
 /**
  * Created by cuiyan on 2018/11/2.
@@ -56,7 +59,6 @@ public class PersonalInfoEditActivity extends BaseActivity implements View.OnCli
                 break;
             }
             case R.id.tv_user_birthday: {
-//                PickerUtil.showDatePicker();
                 Calendar calendar = Calendar.getInstance(Locale.CHINESE);
                 int[] startDate = {calendar.get(Calendar.YEAR) - Constants.MAX_AGE, calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DATE)};
                 int[] selectedDate = {calendar.get(Calendar.YEAR) - 18, 1, 1};
@@ -64,18 +66,42 @@ public class PersonalInfoEditActivity extends BaseActivity implements View.OnCli
                 PickerUtil.showDatePicker(startDate, endDate, selectedDate, this, new DatePicker.OnYearMonthDayPickListener() {
                     @Override
                     public void onDatePicked(String year, String month, String day) {
-
+                        setText(R.id.tv_user_birthday, year + "-" + month + "-" + day);
                     }
                 });
                 break;
             }
             case R.id.tv_user_gender: {
+                final List<String> genderList = Arrays.asList(getResources().getStringArray(R.array.gender_array));
+                PickerUtil.showSinglePicker(this, genderList, new SinglePicker.OnItemPickListener<String>() {
+                    @Override
+                    public void onItemPicked(int index, String item) {
+                        setText(R.id.tv_user_gender, genderList.get(index));
+                    }
+                });
                 break;
             }
             case R.id.tv_user_grade: {
+                final List<String> gradeList = Arrays.asList(getResources().getStringArray(R.array.grade_array));
+                PickerUtil.showSinglePicker(this, gradeList, new SinglePicker.OnItemPickListener<String>() {
+                    @Override
+                    public void onItemPicked(int index, String item) {
+                        setText(R.id.tv_user_grade, gradeList.get(index));
+                    }
+                });
                 break;
             }
             case R.id.tv_college_start_year: {
+                Calendar calendar = Calendar.getInstance(Locale.CHINESE);
+                int[] startDate = {calendar.get(Calendar.YEAR) - Constants.MAX_AGE, calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DATE)};
+                int[] selectedDate = {calendar.get(Calendar.YEAR) - 18, 1, 1};
+                int[] endDate = {calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DATE)};
+                PickerUtil.showYearMonthPicker(startDate, endDate, selectedDate, this, new DatePicker.OnYearMonthPickListener() {
+                    @Override
+                    public void onDatePicked(String year, String month) {
+                        setText(R.id.tv_college_start_year, year + "-" + month);
+                    }
+                });
                 break;
             }
         }
--- app/src/main/java/com/suixue/edu/college/util/PickerUtil.java
+++ app/src/main/java/com/suixue/edu/college/util/PickerUtil.java
@@ -1,11 +1,17 @@
 package com.suixue.edu.college.util;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
 import android.support.annotation.Size;
+import android.view.View;
 
 import com.dev.kit.basemodule.util.DisplayUtil;
 
+import java.util.List;
+
 import cn.qqtheme.framework.picker.DatePicker;
+import cn.qqtheme.framework.picker.SinglePicker;
+import cn.qqtheme.framework.picker.WheelPicker;
 
 /**
  * Created by cuiyan on 2018/11/5.
@@ -22,6 +28,27 @@ public class PickerUtil {
         picker.setResetWhileWheel(false);
         picker.setLineSpaceMultiplier(3);
         picker.setOnDatePickListener(listener);
+        picker.show();
+    }
+
+    public static void showYearMonthPicker(@Size(3) int[] startDate, @Size(3) int[] endDate, @Size(3) int[] selectedDate, Activity activity, DatePicker.OnYearMonthPickListener listener) {
+        final DatePicker picker = new DatePicker(activity, DatePicker.YEAR_MONTH);
+        picker.setCanceledOnTouchOutside(true);
+        picker.setUseWeight(true);
+        picker.setTopPadding(DisplayUtil.dp2px(10));
+        picker.setRangeStart(startDate[0], startDate[1], startDate[2]);
+        picker.setRangeEnd(endDate[0], endDate[1], endDate[2]);
+        picker.setSelectedItem(selectedDate[0], selectedDate[1]);
+        picker.setResetWhileWheel(false);
+        picker.setLineSpaceMultiplier(3);
+        picker.setOnDatePickListener(listener);
+        picker.show();
+    }
+
+    public static <T> void showSinglePicker(Activity activity, List<T> dataList, SinglePicker.OnItemPickListener<T> listener) {
+        SinglePicker<T> picker = new SinglePicker<>(activity, dataList);
+        picker.setLineSpaceMultiplier(3);
+        picker.setOnItemPickListener(listener);
         picker.show();
     }
 }
